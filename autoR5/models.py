ea5a1fb0f567ab786afa094380df756e290dd9f9
Ali <alissaeid@live.co.uk>
2023-10-20 19:31:09 +0000

    removed reciever import from models.py

--- autoR5/models.py
+++ autoR5/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.urls import reverse
 from django.utils import timezone
 from cloudinary.models import CloudinaryField
